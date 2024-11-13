c642f27c327a379285685a5a52b7a82abff30ff0
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-13 23:26:47 +0200

    feat initial commit

--- flight_planner/storage.py
+++ flight_planner/storage.py
@@ -1,5 +1,4 @@
-from general_functions import create_entity, get_entity, delete_entity, delete_all_entities, search_entities, \
-    update_entity
+import general_functions
 
 cities = {}
 airports = {}
@@ -8,7 +7,7 @@ flights = {}
 
 # Functions for cities
 def create_city(city):
-    return create_entity(cities, city)
+    return general_functions.create_entity(cities, city)
 
 
 def get_all_cities():
@@ -16,20 +15,20 @@ def get_all_cities():
 
 
 def get_city(city_id):
-    return get_entity(cities, city_id)
+    return general_functions.get_entity(cities, city_id)
 
 
 def delete_city(city_id):
-    delete_entity(cities, city_id)
+    general_functions.delete_entity(cities, city_id)
 
 
 def delete_all_cities():
-    delete_all_entities(cities)
+    general_functions.delete_all_entities(cities)
 
 
 #Functions for airports
 def create_airport(airport):
-    return create_entity(airports, airport)
+    return general_functions.create_entity(airports, airport)
 
 
 def get_all_airports():
@@ -37,24 +36,24 @@ def get_all_airports():
 
 
 def get_airport(airport_id):
-    return get_entity(airports, airport_id)
+    return general_functions.get_entity(airports, airport_id)
 
 
 def delete_airport(airport_id):
-    delete_entity(airports, airport_id)
+    general_functions.delete_entity(airports, airport_id)
 
 
 def delete_all_airports():
-    delete_all_entities(airports)
+    general_functions.delete_all_entities(airports)
 
 
 def update_airport(airport, airport_id, airport_data):
-    return update_entity(airport, airport_id, airport_data)
+    return general_functions.update_entity(airport, airport_id, airport_data)
 
 
 #Functions for flights
 def create_flight(flight):
-    return create_entity(flights, flight)
+    return general_functions.create_entity(flights, flight)
 
 
 def get_all_flights():
@@ -62,20 +61,20 @@ def get_all_flights():
 
 
 def get_flight(flight_id):
-    return get_entity(flights, flight_id)
+    return general_functions.get_entity(flights, flight_id)
 
 
 def delete_flight(flight_id):
-    delete_entity(flights, flight_id)
+    general_functions.delete_entity(flights, flight_id)
 
 
 def delete_all_flights():
-    delete_all_entities(flights)
+    general_functions.delete_all_entities(flights)
 
 
 def search_flight(flights, criteria):
-    return search_entities(flights, criteria)
+    return general_functions.search_entities(flights, criteria)
 
 
-def update_flight(flight, flight_id,  flight_data):
-    return update_entity(flight, flight_id, flight_data)
+def update_flight(flight, flight_id, flight_data):
+    return general_functions.update_entity(flight, flight_id, flight_data)
