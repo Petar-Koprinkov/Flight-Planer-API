8f14601952f124b8e72ed74b467b678a886f2ecb
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-14 10:06:56 +0200

    feat: Added typing

--- flight_planner/services.py
+++ flight_planner/services.py
@@ -1,36 +1,37 @@
 import storage
+from typing import Callable, Union
 
 
 class CityService:
     @staticmethod
-    def create_city(city):
+    def create_city(city: str) -> Callable:
         return storage.create_city(city)
 
     @staticmethod
-    def get_all_cities():
+    def get_all_cities() -> Callable:
         return storage.get_all_cities()
 
     @staticmethod
-    def get_city(city_id):
+    def get_city(city_id: int) -> Union[str, Exception]:
         city = storage.get_city(city_id)
         if city is None:
             raise KeyError(f"City with id {city_id} not found!")
         return city
 
     @staticmethod
-    def delete_city(city_id):
+    def delete_city(city_id: int) -> None:
         storage.delete_city(city_id)
         return ''
 
     @staticmethod
-    def delete_all_cities():
+    def delete_all_cities() -> None:
         storage.delete_all_cities()
         return ''
 
 
 class AirportService:
     @staticmethod
-    def create_airport(airport):
+    def create_airport(airport: str) -> Callable:
         return storage.create_airport(airport)
 
     @staticmethod
@@ -38,63 +39,62 @@ class AirportService:
         return storage.get_all_airports()
 
     @staticmethod
-    def get_airport(airport_id):
+    def get_airport(airport_id: int) -> Union[str, Exception]:
         airport = storage.get_airport(airport_id)
         if airport is None:
             raise KeyError(f"Airport with id {airport_id} not found!")
         return airport
 
     @staticmethod
-    def delete_airport(airport_id):
+    def delete_airport(airport_id: int) -> None:
         storage.delete_airport(airport_id)
         return ''
 
     @staticmethod
-    def delete_all_airports():
+    def delete_all_airports() -> None:
         storage.delete_all_airports()
         return ''
 
     @staticmethod
-    def update_airport(airport, airport_id,  airport_data):
-        return storage.update_flight(airport, airport_id,  airport_data)
+    def update_airport(airport: str, airport_id: int, airport_data: str) -> Callable:
+        return storage.update_flight(airport, airport_id, airport_data)
 
     @staticmethod
-    def update_all_airports(airport, airport_criteria):
+    def update_all_airports(airport: str, airport_criteria: str) -> Callable:
         return storage.update_all_airports(airport, airport_criteria)
-
 
 
 class FlightService:
 
     @staticmethod
-    def create_flight(flight):
+    def create_flight(flight: str) -> Callable:
         return storage.create_flight(flight)
 
     @staticmethod
-    def get_all_flights():
+    def get_all_flights() -> Callable:
         return storage.get_all_flights()
 
     @staticmethod
-    def get_flight(flight_id):
+    def get_flight(flight_id: int) -> Union[str, Exception]:
         flight = storage.get_flight(flight_id)
         if flight is None:
             raise KeyError(f"Flight with id {flight_id} not found!")
         return flight
 
     @staticmethod
-    def delete_flight(flight_id):
+    def delete_flight(flight_id: int) -> None:
         storage.delete_flight(flight_id)
         return ''
 
     @staticmethod
-    def delete_all_flights():
+    def delete_all_flights() -> None:
         storage.delete_all_flights()
         return ''
 
     @staticmethod
-    def search_flights(flights, flight_criteria):
+    def search_flights(flights: str, flight_criteria: str) -> Callable:
         return storage.search_flight(flights, flight_criteria)
 
     @staticmethod
-    def update_flight(flight, flight_id,  flight_data):
+    def update_flight(flight: str, flight_id: int, flight_data: str) -> Callable:
         return storage.update_flight(flight, flight_id, flight_data)
--- flight_planner/storage.py
+++ flight_planner/storage.py
@@ -1,3 +1,5 @@
+from typing import Callable, Dict, List
+
 import general_functions
 
 cities = {}
@@ -6,79 +8,79 @@ flights = {}
 
 
 # Functions for cities
-def create_city(city):
+def create_city(city: str) -> Dict:
     return general_functions.create_entity(cities, city)
 
 
-def get_all_cities():
+def get_all_cities() -> List:
     return list(cities.values())
 
 
-def get_city(city_id):
+def get_city(city_id) -> str:
     return general_functions.get_entity(cities, city_id)
 
 
-def delete_city(city_id):
+def delete_city(city_id) -> None:
     general_functions.delete_entity(cities, city_id)
 
 
-def delete_all_cities():
+def delete_all_cities() -> None:
     general_functions.delete_all_entities(cities)
 
 
 #Functions for airports
-def create_airport(airport):
+def create_airport(airport) -> Dict:
     return general_functions.create_entity(airports, airport)
 
 
-def get_all_airports():
+def get_all_airports() -> List:
     return list(airports.values())
 
 
-def get_airport(airport_id):
+def get_airport(airport_id) -> str:
     return general_functions.get_entity(airports, airport_id)
 
 
-def delete_airport(airport_id):
+def delete_airport(airport_id) -> None:
     general_functions.delete_entity(airports, airport_id)
 
 
-def delete_all_airports():
+def delete_all_airports() -> None:
     general_functions.delete_all_entities(airports)
 
 
-def update_airport(airport, airport_id, airport_data):
+def update_airport(airport, airport_id, airport_data) -> Dict:
     return general_functions.update_entity(airport, airport_id, airport_data)
 
 
-def update_all_airports(airports, criteria):
+def update_all_airports(airports, criteria) -> Dict:
     return general_functions.update_all_entities(airports, criteria)
 
 
 #Functions for flights
-def create_flight(flight):
+def create_flight(flight) -> Dict:
     return general_functions.create_entity(flights, flight)
 
 
-def get_all_flights():
+def get_all_flights() -> List:
     return list(flights.values())
 
 
-def get_flight(flight_id):
+def get_flight(flight_id) -> str:
     return general_functions.get_entity(flights, flight_id)
 
 
-def delete_flight(flight_id):
+def delete_flight(flight_id) -> None:
     general_functions.delete_entity(flights, flight_id)
 
 
-def delete_all_flights():
+def delete_all_flights() -> None:
     general_functions.delete_all_entities(flights)
 
 
-def search_flight(flights, criteria):
+def search_flight(flights, criteria) -> str:
     return general_functions.search_entities(flights, criteria)
 
 
-def update_flight(flight, flight_id, flight_data):
+def update_flight(flight, flight_id, flight_data) -> Dict:
     return general_functions.update_entity(flight, flight_id, flight_data)
