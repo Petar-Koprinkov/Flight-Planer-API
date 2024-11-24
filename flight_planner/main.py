0f6e6eb49aa8dc2f4950139093488d79ca8d9d40
Petar Koprinkov <petur_koprinkov@abv.bg>
2024-11-24 12:29:42 +0200

    fix

--- flight_planner/main.py
+++ flight_planner/main.py
@@ -5,7 +5,15 @@ from services import FlightService
 
 def main():
     # Insert CRUD operations
-    pass
+    new_flight = {
+        "flight_number": "BA250",
+        "origin": "LHR",
+        "destination": "DXB",
+        "departure_time": "2024-12-10T15:00:00",
+        "arrival_time": "2024-12-10T23:00:00"
+    }
+
+    FlightService.create_flight(new_flight)
 
 
 if __name__ == "__main__":
